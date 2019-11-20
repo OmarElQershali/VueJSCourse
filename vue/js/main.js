915c64ac98524a34d91cb1ab6afa16eb01edfeb1
Omar <omerkurshali@gmail.com>
2019-11-20 14:56:22 +0200

    update

--- vue/js/main.js
+++ vue/js/main.js
@@ -10,28 +10,67 @@
  * methods
  * computed
  * watch
- *
+ * Loops
+ * Multiple vue instance
  */
+Vue.component("greeting", {
+  template: "<p>this is first component with {{ name }} </p>",
+  data: function() {
+    return {
+      name: "Omar"
+    };
+  },
+  methods: {}
+});
 
-new Vue({
+var MainInstance = new Vue({
   el: "#app",
   data: {
     name: "",
     age: "",
     message: "Welcome to new course Vue js :)",
     website: "www.DevO.com",
-    email: "omerkurshali@gmail.com"
-  },
-  methods: {
-    greeting: function() {
-      return "HI to new Course";
-    }
-    /*AddAge: function() {
+    email: "omerkurshali@gmail.com",
+    NameLists: ["Omar", "mohammad", "khames", "fawze"],
+    JobsLists: [
+      { name: "Developer", salary: "500$" },
+      { name: "Designer", salary: "400$" },
+      { name: "ProjectManager", salary: "800$" }
+    ],
+    methods: {
+      /*AddAge: function() {
       this.age++;
     },
     SubAge: function() {
       this.age--;
     }*/
+    },
+    computed: {}
+  }
+});
+
+var TestInstance1 = new Vue({
+  el: "#FirstTest",
+  data: {
+    title: "Hi in First Test",
+    decripions: "this is first decripion "
   },
-  computed: {}
+  methods: {},
+  computed: {},
+  watch: {}
+});
+
+var TestInstance2 = new Vue({
+  el: "#SecondTest",
+  data: {
+    title: "Hi in Second Test",
+    decripions: "this is Second decripion "
+  },
+  methods: {},
+  computed: {
+    ChangeDescripion: function() {
+      TestInstance1.decripions = "decripion Changed";
+    }
+  },
+  watch: {}
 });
